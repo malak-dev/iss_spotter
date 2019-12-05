4e8335f8bd5d70267f10d39af5a237c7a8e2730a
malak <malak-y@live.com>
2019-12-05 23:23:17 +0000

    ISS Spotter II

---
+++ iss.js
@@ -0,0 +1,37 @@
+
+const request = require('request');
+const fetchMyIP = function(callback) {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const ip = JSON.parse(body);
+    callback(null, ip.ip);
+
+  });
+};
+const fetchCoordsByIP = function(ip, callback) {
+  request('https://ipvigilante.com/8.8.8.8', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+    callback(null, { latitude, longitude });
+
+  });
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
